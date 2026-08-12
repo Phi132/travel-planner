03af0a2b982676cd84683ee2429205f30cb6b155
phi132 <tphi10761@gmail.com>
2026-08-12 21:19:51 +0700

    fix frontend api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/useAuthStore';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
   withCredentials: true // để trình duyệt gửi kèm cookie refreshToken
 });
 
